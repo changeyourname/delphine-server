a02152a72f62a696b6289f2766d821a81f8ec5b4
Daan Vos de Wael <daan@vosdewael.com>
2015-04-19 19:12:17 +0200

    refactoring to get the reddit module 100% coverage

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 var fetch = require('../lib/fetch');
 
-describe.only('fetch', function() {
+describe('fetch', function() {
   afterEach(function(done) {
     request.get.restore();
     done();
@@ -39,7 +39,7 @@ describe.only('fetch', function() {
     it('should return 404 when page not found', function(done) {
       fetch('http://daan.vosdewael.com/404', function(err, result) {
         expect(request.get.called).to.be.true;
-        expect(err).to.be.null;
+        expect(err).to.be.true;
         expect(result).to.be.empty;
         done();
       });
--- test/reddit.test.js
+++ test/reddit.test.js
@@ -5,73 +5,85 @@ var request = require('request');
 var reddit = require('../lib/reddit');
 
 describe('reddit', function() {
-  before(function(done) {
-    sinon
-      .stub(request, 'get')
-      .yields(null, null, JSON.stringify({
-        data: {
-          children: [
-            {
-              data: {
-                id: '1234',
-                author: 'foobar',
-                domain: 'awesome.domain.com',
-                is_self: false,
-                subreddit: 'subreddit',
-                thumbnail: 'http://awesome.thumbnail.com/',
-                title: 'title',
-                url: 'http://awesome.url.com/'
-              }
-            },
-            {
-              data: {
-                id: '1234',
-                author: 'foobar',
-                domain: 'awesome.domain.com',
-                is_self: true,
-                subreddit: 'subreddit',
-                thumbnail: 'http://awesome.thumbnail.com/',
-                title: 'title',
-                url: 'http://awesome.url.com/'
-              }
-            }
-          ]
-        }
-      }));
-    done();
-  });
-
-  after(function(done) {
+  afterEach(function(done) {
     request.get.restore();
     done();
   });
 
-  it('should have the correct count', function(done) {
-    reddit('http://reddit.feed.com/', function(err, items) {
-      if (err) return done(err);
-      expect(items).to.have.length(1);
+  describe('200 OK', function() {
+    beforeEach(function(done) {
+      sinon
+        .stub(request, 'get')
+        .yields(null, { statusCode: 200 }, JSON.stringify({
+          data: {
+            children: [
+              {
+                data: {
+                  id: '1234',
+                  author: 'foobar',
+                  domain: 'awesome.domain.com',
+                  is_self: false,
+                  subreddit: 'subreddit',
+                  thumbnail: 'http://awesome.thumbnail.com/',
+                  title: 'title',
+                  url: 'http://awesome.url.com/'
+                }
+              },
+              {
+                data: {
+                  is_self: true
+                }
+              }
+            ]
+          }
+        }));
       done();
+    });
+
+    it('should call request & have the correct count', function(done) {
+      reddit('https://feed.reddit.com/', function(err, items) {
+        expect(request.get.called).to.be.true;
+        expect(items).to.have.length(1);
+        done();
+      });
+    });
+
+    it('should have only the necessary keys', function(done) {
+      reddit('https://feed.reddit.com/', function(err, items) {
+        expect(items[0]).to.have.all.keys(['id', 'domain', 'subreddit', 'thumbnail', 'title', 'url']);
+        done();
+      });
+    });
+
+    it('should have the correct values', function(done) {
+      reddit('https://feed.reddit.com/', function(err, items) {
+        expect(items[0].id).to.equal('1234');
+        expect(items[0].domain).to.equal('awesome.domain.com');
+        expect(items[0].subreddit).to.equal('subreddit');
+        expect(items[0].thumbnail).to.equal('http://awesome.thumbnail.com/');
+        expect(items[0].title).to.equal('title');
+        expect(items[0].url).to.equal('http://awesome.url.com/');
+        done();
+      });
     });
   });
 
-  it('should have only the necessary keys', function(done) {
-    reddit('http://reddit.feed.com/', function(err, items) {
-      if (err) return done(err);
-      expect(items[0]).to.have.all.keys(['id', 'domain', 'subreddit', 'thumbnail', 'title', 'url']);
+  describe('404 Not Found', function() {
+    before(function(done) {
+      sinon
+        .stub(request, 'get')
+        .yields(null, { statusCode: 404 }, null);
       done();
+    });
+
+    it('should return 404 when page not found', function(done) {
+      reddit('http://daan.vosdewael.com/404', function(err, items) {
+        expect(request.get.called).to.be.true;
+        expect(err).to.be.true;
+        expect(items).to.be.empty;
+        done();
+      });
     });
   });
 
-  it('should have the correct values', function(done) {
-    reddit('http://reddit.feed.com/', function(err, items) {
-      if (err) return done(err);
-      expect(items[0].id).to.equal('1234');
-      expect(items[0].domain).to.equal('awesome.domain.com');
-      expect(items[0].subreddit).to.equal('subreddit');
-      expect(items[0].thumbnail).to.equal('http://awesome.thumbnail.com/');
-      expect(items[0].title).to.equal('title');
-      expect(items[0].url).to.equal('http://awesome.url.com/');
-      done();
-    });
-  });
 });
