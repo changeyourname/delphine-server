47ff15030306f0862721705f29bd6c16d08645ba
Daan Vos de Wael <daan@vosdewael.com>
2015-05-04 00:23:34 +0200

    replaced hardcoded Reddit url for env variable

--- test/reddit.test.js
+++ test/reddit.test.js
@@ -1,11 +1,14 @@
+require('dotenv').load();
 var expect = require('chai').expect;
 var nock = require('nock');
 var path = require('path');
+var url = require('url');
 
 var redis = require('redis');
 var client = redis.createClient();
 
 var reddit = require('../lib/reddit');
+var redditURL = url.parse(process.env.REDDIT_URL);
 
 describe('reddit', function() {
   afterEach(function(done) {
@@ -17,27 +20,27 @@ describe('reddit', function() {
   describe('200 OK', function() {
     beforeEach(function() {
       nock.cleanAll();
-      nock('https://www.reddit.com')
-        .get('/.json?feed=some_random_id')
+      nock(redditURL.protocol + '//' + redditURL.host)
+        .get(redditURL.pathname + redditURL.search)
         .replyWithFile(200, path.join(__dirname, '/replies/reddit.json'));
     });
 
     it('should have the correct item count', function(done) {
-      reddit('https://www.reddit.com/.json?feed=some_random_id', function(err, items) {
+      reddit(function(err, items) {
         expect(items).to.have.length(24);
         done();
       });
     });
 
     it('should have only the necessary keys', function(done) {
-      reddit('https://www.reddit.com/.json?feed=some_random_id', function(err, items) {
+      reddit(function(err, items) {
         expect(items[0]).to.have.all.keys(['id', 'domain', 'subreddit', 'thumbnail', 'title', 'url']);
         done();
       });
     });
 
     it('should not return undefined values', function(done) {
-      reddit('https://www.reddit.com/.json?feed=some_random_id', function(err, items) {
+      reddit(function(err, items) {
         expect(items[0].id).to.not.be.undefined;
         expect(items[0].domain).to.not.be.undefined;
         expect(items[0].subreddit).to.not.be.undefined;
@@ -50,7 +53,7 @@ describe('reddit', function() {
 
     it('should return data from redis', function(done) {
       client.set('reddit', JSON.stringify([{ id: '33tgeb', domain: 'i.imgur.com', subreddit: 'pics', thumbnail: 'http://a.thumbs.redditmedia.com/l25TyZXPMIThqIizldYvh4oYJyKCw0AEcQFrJBLCCS4.jpg', title: 'Oh. You\'re home early.', url: 'http://i.imgur.com/Lab4Ray.jpg' }]), function() {
-        reddit('https://www.reddit.com/.json?feed=some_random_id', function(err, items) {
+        reddit(function(err, items) {
           expect(items[0]).to.have.all.keys(['id', 'domain', 'subreddit', 'thumbnail', 'title', 'url']);
           expect(items[0].id).to.equal('33tgeb');
           expect(items[0].domain).to.equal('i.imgur.com');
@@ -67,13 +70,13 @@ describe('reddit', function() {
   describe('404 Not Found', function() {
     beforeEach(function() {
       nock.cleanAll();
-      nock('https://www.reddit.com')
-        .get('/.json?feed=some_random_id')
+      nock(redditURL.protocol + '//' + redditURL.host)
+        .get(redditURL.pathname + redditURL.search)
         .reply(404, null);
     });
 
     it('should return 404 when page not found', function(done) {
-      reddit('https://www.reddit.com/.json?feed=some_random_id', function(err, items) {
+      reddit(function(err, items) {
         expect(err).to.be.true;
         expect(items).to.be.empty;
         done();
