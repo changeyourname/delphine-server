2b464fedb5862e82018aa22105edeffd2954ae95
Daan Vos de Wael <daan@vosdewael.com>
2015-05-04 00:42:19 +0200

    making the server ready for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var batch = require('./lib/batch');
 
 app.set('port', process.env.PORT || 8080);
 app.disable('x-powered-by');
 
 app.get('/', function(req, res) {
-  res.json({ message: '200 OK' });
+  res.set('Content-Type', 'text/plain');
+  res.send('This is not the page you are looking for.');
+});
+
+app.get('/data', function(req, res) {
+  batch(function(err, items) {
+    if (err) return res.json(err);
+    res.json(items);
+  });
 });
 
 app.listen(app.get('port'));
